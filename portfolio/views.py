7ca07723a9da797c7a63e104ed858b1af886b497
navya mullapudi <navyamullapudi4@gmail.com>
2020-10-11 20:03:10 +0100

    Modified the ui of the page

--- portfolio/views.py
+++ portfolio/views.py
@@ -4,4 +4,4 @@ from .models import Project
 # Create your views here.
 def home(request):
   projects =Project.objects.all()
-  return render(request, 'portfolio/home.html', {'dbkeys': projects})
+  return render(request, 'portfolio/home.html', {'projects': projects})
